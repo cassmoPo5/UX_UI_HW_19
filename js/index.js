a48023a541dff25423165e00c573be65ed0c8866
cassmoPo5 <77122000+cassmoPo5@users.noreply.github.com>
2021-07-27 04:11:00 -0400

    Update

--- js/index.js
+++ js/index.js
@@ -1,23 +1,9 @@
-$(document).ready(function(){
-
-    var $btn = $('#btnSubmit'),
-        $fields = $('.form-control');
-    $btn.on('click', function(){
-      
-      $fields.each(function(){
-        var value = $(this).val();
-        if (value == '') {
-          $(this).addClass('error');
-          $(this).parent().find('.validation').show(1000);
-        } else {
-          $(this).removeClass('error');
-          $(this).parent().find('.validation').hide(1000);
-        }
-      });
-      var errorCount = $('.error').length;
-      if (errorCount == 0) {
-        $btn.prop('disabled', true).text('Loading...');
-      }
-    });
-    
-  });
+{
+  $('.card').appendTo('.hidden');
+  $('.hidden').contents().appendTo('#card').hide().show('slow');
+  $('.card:not(.' + filter + ')').appendTo('.hidden').hide('slow');
+  $( "#filter button" ).removeClass('active');
+  $(this).addClass('active');
+  $("#filter button").attr("disabled", false);
+  $(this).attr("disabled", true);
+}
